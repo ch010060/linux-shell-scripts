6c177389b77d0026f516e11eae9e64960add3f92
ch010060 <ch010060@gmail.com>
2021-07-16 11:53:04 +0800

    Add charset convertion tool.

---
+++ script/Convert_Charset2UTF8.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+OIFS="$IFS"
+IFS=$'\n'
+FILE_EXT="cue"
+for file in $(find . -type f -name "*.${FILE_EXT}")
+do
+     charset=$(uchardet "${file}")
+     if [ "${charset}" != 'UTF-8' ]
+     then
+        iconv -f ${charset} -t utf-8 ${file}
+     fi
+done
+IFS="$OIFS"
